10cce15fea92f942c9e994fbca6b1298b394eb0b
godmar <godmar>
2006-04-24 03:19:40 +0000

    - added comments - changed sequence of print O statements to use print O <<LABEL; syntax
    Changes to the makelive.sh script that's created to account for new libx.org design:
        - now copies config file and file for small icon to release directory
        - created .htaccess file in release directory if it does not exist
        - dump .htaccess file to stdout for user to double-check

--- src/editions/createextension.pl
+++ src/editions/createextension.pl
@@ -19,7 +19,7 @@ while (<H>) {
     next if (/^#/ || /^\s*$/);
     chomp;
     chop if (/\r$/);
-    if (/^\$(.*)$/) {
+    if (/^\$(.*)$/) {           # line starts with a $
         $addprop = $addprop . "\n" . $1;
     } else {
         my ($key, $value) = split(/=/, $_, 2);
@@ -37,7 +37,7 @@ sub copyandreplace {
     #print "processing $src to $dst\n";
     local (*FS);
     open (FS, "<$src") || die "Could not open $src for reading";
-    my $srctext = do { local ($/); <FS> };
+    my $srctext = do { local ($/); <FS> };      # slurp
     close (FS);
     foreach $key (keys(%conf)) {
         $srctext =~ s/\$$key\$/$conf{$key}/g;
@@ -80,18 +80,49 @@ foreach $file (keys(%files)) {
 
 my $addtoplevelfiles = "install.rdf changelog.txt chrome.manifest";
 my $xpifile = $conf{'xpilocation'};
-$xpifile =~ s/.*\/([^\/]*)/$1/;
+$xpifile =~ s/.*\/([^\/]*)/$1/;         # basename
 system("cp tmp/update.rdf $edition");
 system("cd tmp; rm ../$edition/$xpifile; find . -name CVS -type dir | xargs /bin/rm -fr ; zip -r ../$edition/$xpifile ./chrome " . $addtoplevelfiles);
 
 my $livescript = "$edition/makelive.sh";
 open (O, ">$livescript") || die ("Could not write to $livescript");
-print O "#!/bin/sh\n";
-print O "T=$copytargetdir/$edition\n";
-print O "test -d \$T || (echo \"This works only on a machine where \$T exists\"; exit)\n";
-print O "cp $xpifile \$T\n";
-print O "cp update.rdf \$T\n";
-print O "test -r libx.html && cp libx.html \$T\n";
+
+my $icon = $conf{'emiconURL'};
+$icon =~ s/.*\/([^\/]*)/$1/;         # basename of icon
+
+print O <<MAKELIVE_SCRIPT;
+#!/bin/sh
+T=$copytargetdir/$edition
+test -d \$T || echo This works only on a machine where \$T exists
+test -d \$T || exit
+cp $xpifile \$T
+cp update.rdf \$T
+cp $icon \$T
+cp config \$T
+
+echo I have copied the files for this edition to the $copytargetdir/$edition/ directory
+
+test -r \$T/.htaccess && echo \$T/.htaccess exists, please check for correctness of edition URL
+test -r \$T/.htaccess || echo No \$T/.htaccess file found, will create a default one.
+
+test -r \$T/.htaccess || cat > \$T/.htaccess << HTACCESSFILE
+AddType application/x-xpinstall .xpi
+Redirect /editions/$edition/libx.html http://libx.org/editions/download.php?edition=$edition
+HTACCESSFILE
+
+cat << BEGINHTACCESS
+----- begin of .htaccess file -----
+BEGINHTACCESS
+
+test -r \$T/.htaccess && cat \$T/.htaccess
+cat << ENDHTACCESS
+----- end of .htaccess file -----
+
+Do not forget to add this edition to $copytargetdir/editions/editions.list
+if it is to be shown on the libx.org main page
+ENDHTACCESS
+MAKELIVE_SCRIPT
+        
 close (O);
 chmod 0755, "$livescript";
 
