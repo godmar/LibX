0bb6931c2215457c5744a90f8c04c52205fdfa5a
groovydude18 <groovydude18>
2007-06-02 20:29:54 +0000

    Added more comments and fixed a compatability issue.

--- src/base/chrome/libx/content/libx/hover.js
+++ src/base/chrome/libx/content/libx/hover.js
@@ -7,22 +7,41 @@ libxEnv.libxHoverBox = function ( doc, link, persist ) {
     this.linked = link;
     //used for a possible future persistent link
     this.persistent = persist;
-    // methods of object
+    //  ====   methods of object  ====
+//  add is used if you aren't specifically building for things like a cue
+// if you want to add elements to a tooltip/hoverbox that will be used for
+// something else, it is recommended you use this method to allow you
+// easier use with that specific tooltip/hoverbox.  You may also use
+// the addElement but must make sure you use the correct tooltip/hoverbox
+// div
     this.add = function( linkobj )
     {
         libxEnv.addElement(this.menuDiv, linkobj);
     }
-    this.cueHover = function( _title )
+// cueHover is an easy method so that to make a tooltip for a cue, its
+// clean and concise.  Use this method after initializing the hoverObject
+// and everything will be taken care of.
+    this.cueHover = function( )
     {
+        //  First you must create the element that will have all the
+        // monitors associated with it.
         var obj = doc.createElement('a');
-        var text = doc.createTextNode('Search ISBN');
+        //  Add any objects.  In this case you will just add the title
+        // of the original cue to what displays with this, you can change
+        // or add more if wanted
+        var text = doc.createTextNode(this.linked.title);
+        //  You can change more of the attributes of the object ( obj )
+        // so that it will display other things or have other behaviors.
+        // in this case, the href is taken from the original cue and added
+        // to the tooltip/hoverbox element in question.
         obj.href = this.linked.href;
+        //  Now the text element is added to the object which will become
+        // an element of the tooltip/hover box.
         obj.appendChild(text);
+        //  After constructing the element to be added, you hand it off
+        // to the addElement method.  This gives you easy access to add
+        // and element (obj) to a specific tooltip/hoverbox (this.menuDiv)
         libxEnv.addElement( this.menuDiv, obj );
-        var obj2 = doc.createElement('a');
-        var text2 = doc.createTextNode('Search by Title');
-        obj2.appendChild(text2);
-        libxEnv.addElement( this.menuDiv, obj2 );
     }
 
     this.menuDiv = libxEnv.makeHoverBox(this.doc, this.linked);
@@ -32,7 +51,10 @@ libxEnv.libxHoverBox = function ( doc, link, persist ) {
 // Initializer function to initialize the timeout and the hover object
 libxEnv.hoverInit = function ()
 {
+    // give space to be used to store tooltip/hoverbox attributes
     libxEnv.HoverObject = new Object();
+    // delay for how quickly the tooltip/hoverbox will disappear
+    // on mouseout
     libxEnv.HoverObject.disappeardelay = 250;
 
 }
@@ -41,16 +63,21 @@ libxEnv.hoverInit = function ()
 // the menu is the object, and the linkObj is the element to be added
 libxEnv.addElement = function(object, linkObj)
 {
-	
+    //  set mouse over to clear any hiding of the menu
+    // this case takes care of the possibility of leaving the dropdown
+    // part and coming back
     linkObj.onmouseover = function() { libxEnv.clearhidemenu(); };
+    //  Makes the entire dropdown disspear in case of being clicked on
     linkObj.onclick = function() { libxEnv.hidemenu(linkObj); };
+    //  adds the linkobj to the tooltip/hoverbox
     object.appendChild( linkObj );
 }
 
 // A way to initially make the hover box and bringing in the stylesheet for
-// user by the box to allow easy modifications
+// use by the box to allow easy modifications
 libxEnv.makeHoverBox = function(doc, linkObj)
 {
+    // add style sheet to the page for use in building the tooltip/hoverbox
     if(doc.createStyleSheet) 
     {
         doc.createStyleSheet('chrome://libx/skin/hover.css');
@@ -62,6 +89,8 @@ libxEnv.makeHoverBox = function(doc, linkObj)
         newSS.href= "chrome://libx/skin/hover.css";
         libxEnv.xpath.findSingle( doc, "//head").appendChild(newSS);
     }
+    // create the div to be used by the script that will act as the
+    // container for all drop down menus. 
     var hoverDiv = doc.createElement('div');
 
     linkObj.setAttribute('hover', "hover_over");
@@ -99,7 +128,7 @@ libxEnv.dropMenu = function (object, ele)
         totaloffset = totaloffset + parentElement.offsetLeft;
         parentElement = parentElement.offsetParent;
     }
-    ele.style.left = totaloffset;
+    ele.style.left = totaloffset + 'px';
     totaloffset = object.offsetTop;
     parentElement = object.offsetParent;
     while (parentElement != null)
@@ -107,7 +136,7 @@ libxEnv.dropMenu = function (object, ele)
         totaloffset = totaloffset + parentElement.offsetTop;
         parentElement = parentElement.offsetParent;
     }
-    ele.style.top = totaloffset + object.offsetHeight;
+    ele.style.top = totaloffset + object.offsetHeight + 'px';
 }
 
 // This function will hide the ele passed into it
@@ -123,3 +152,4 @@ libxEnv.clearhidemenu = function ()
 {
     clearTimeout(libxEnv.HoverObject.delayhide);
 }
+
