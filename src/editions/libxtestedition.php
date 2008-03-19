4c356b6745ef0cf17defd71be25f947a68d9d345
godmar <godmar>
2008-03-19 21:40:03 +0000

    updates regarding backwards compatibility

--- src/editions/libxtestedition.php
+++ src/editions/libxtestedition.php
@@ -35,18 +35,26 @@ pclose($ls);
 ?>
 </ul>
 <p>
-We are currently switching to the new 
+If you are a user seeing this page after coming from the 
+<a href="/editions.php">Editions</a> list, then this means that the maintainer
+of this edition has made the edition public, but has not made it live.
+Contact him or her and ask for the edition to be made live.
+To find the maintainer, visit the
 <a href="http://libx.org/editionbuilder">edition builder</a> system.
-Your old test pages have been moved to the links above.
 <p>
+Before the switch to the new 
+<a href="http://libx.org/editionbuilder">edition builder</a> system,
+this URL was used to lead to an edition's test page.
+It is now only here for backwards compatibility.
+<p>
+<b>Transition note for maintainers:</b>
 If your edition was a "live edition", revision #1 will be the live edition 
 and revision #2 will
 be your current test edition.  If not, your current test edition is revision #1.
 To maintain your edition, sign up for an account, then select the edition in the
 "All Editions" tab, then "Request Ownership".
 <p>
-The edition builder is brand new code (as of 7/10/07), reports bugs or any 
-problems and suggestions to libx.editions@gmail.com
+Reports bugs or any problems and suggestions to libx.editions@gmail.com
 <p>
 
 </body>
