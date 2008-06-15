d607fd1899c00e5d6c0381beff81cf55d27b5520
godmar <godmar>
2008-06-15 04:21:01 +0000

    added autolink to 'doforurl'

--- src/feeds/cues/autolink.js
+++ src/feeds/cues/autolink.js
@@ -1,5 +1,5 @@
 
-var autolink = new libxEnv.doforurls.DoForURL(/.*/, function (doc) {
+var autolink = libxEnv.doforurls.autolink = new libxEnv.doforurls.DoForURL(/.*/, function (doc) {
     // to work around https://bugzilla.mozilla.org/show_bug.cgi?id=315997
     // we skip autolink if the page contains any textarea element.
     // (though the bug purportedly only affects large textarea elements.)
