123541cc79accaf4ebc54ba2ad4e7bac3fc7d769
godmar <godmar>
2008-09-22 13:28:14 +0000

    added excludes for MIT and UChicago who don't wish COinS processing on certain sites

--- src/feeds/cues/coins.js
+++ src/feeds/cues/coins.js
@@ -1,6 +1,13 @@
 if (libxEnv.openUrlResolver && libxEnv.options.supportcoins) {
+    /* MIT doesn't want coins on mit.worldcat.org --- exclude this site for now
+     * XXX find better solution.
+     */
+    var exclude = [ /mit\.worldcat\.org/, 
+                    /lens\.lib\.uchicago\.edu/ 
+                  ];
+
     new libxEnv.doforurls.DoForURL(/.+/, 
                  function (doc) {
                      libxEnv.handleCoins(doc, libxEnv.openUrlResolver) 
-                 });
+                 }, exclude);
 }
