cf1f0a46eeb346f758d21ea1df92bf5e0447683c
vtdoylem <vtdoylem>
2008-11-11 20:13:52 +0000

    Changed to be compliant w/ changes to libx restructuring

--- src/feeds/cues/abebooks.js
+++ src/feeds/cues/abebooks.js
@@ -10,9 +10,9 @@ new libxEnv.doforurls.DoForURL(/(\/\/|\.)abebooks\.com/, function (doc) {
         if (isbn) {
             var newlink = libxEnv.makeLink(doc,
                 libxEnv.getProperty("isbnsearch.label",
-                    [libraryCatalog.name, isbn]),
-                libraryCatalog.linkByISBN(isbn), libraryCatalog);
-            createXISBNTooltip(newlink, isbn, libraryCatalog.name);
+                    [libx.edition.catalogs.default.name, isbn]),
+                libx.edition.catalogs.default.linkByISBN(isbn), libx.edition.catalogs.default);
+            createXISBNTooltip(newlink, isbn, libx.edition.catalogs.default.name);
             n[i].parentNode.insertBefore(newlink, n[i].nextSibling);
             n[i].parentNode.insertBefore(doc.createTextNode(" "), newlink);
             animateCue(newlink);
