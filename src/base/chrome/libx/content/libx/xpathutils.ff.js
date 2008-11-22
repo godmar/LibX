140698c1959aa5fcbc0949deacb7d7fa71497900
aikhokar <aikhokar>
2008-11-22 09:13:58 +0000

    1. Removed ability to handle boolean, string or number return types 2. Some refactoring.

--- src/base/chrome/libx/content/libx/xpathutils.ff.js
+++ src/base/chrome/libx/content/libx/xpathutils.ff.js
@@ -30,85 +30,123 @@ libx.ff.xpath = {
     /* See http://developer.mozilla.org/en/docs/Introduction_to_using_XPath_in_JavaScript
      * and http://www.xulplanet.com/references/objref/XPathResult.html
      *
-     * var xpathResult = document.evaluate(xpathExpression, contextNode, namespaceResolver, resultType, result);
+     * var xpathResult = document.evaluate(xpathExpression, 
+     *                                     contextNode, 
+     *                                     namespaceResolver, 
+     *                                     resultType, 
+     *                                     result);
      *
      * Note: namespaceResolver is required if examined XML uses namespaces.
-     * namespaceResolver is a function that returns a name space based on a prefix.
+     * namespaceResolver is an object where the namespace prefix is the key and
+     * the URI is the value.  It's encapsulated in an anonymous function.
      */
 
     /**
+     * Evaluates an XPath expression and returns a single DOM node or null
+     *
+     * @param {DOM Tree} doc               document (used if root is undefined)
+     * @param {String}   xpathexpr         XPath expression
+     * @param {DOM Tree} root              root of DOM to execute search (used
+     *                                     instead of doc if defined)
+     * @param {Object}   namespaceresolver Object keys are namespace prefixes,
+     *                                     values are corresponding URIs
+     *
+     * @returns DOM node or null if not found
      *
      */
     findSingleXML : function (doc, xpathexpr, root, namespaceresolver) {
-        return this.findSingle(doc, xpathexpr, root, namespaceresolver);
-    },
-
-    /**
-     *
-     */
-    findSingle : function (doc, xpathexpr, root, namespaceresolver) {
-        var r = doc.evaluate(xpathexpr, root ? root : doc, 
-                function (prefix) { 
-                    return namespaceresolver[prefix]; 
-                }, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        if (r) 
-            return r.singleNodeValue;
-        return null;
-    },
-
-    /**
-     *
-     */
-    findNodesXML : function (doc, xpathexpr, root, namespaceresolver) {
-        return this.findNodes(doc, xpathexpr, root, namespaceresolver);
-    },
-
-    /**
-     *
-     */
-    findNodes : function (doc, xpathexpr, root, namespaceresolver) {
-        var r = doc.evaluate(xpathexpr, root ? root : doc, 
-                            function (prefix) { 
-                                return namespaceresolver[prefix]; 
-                            }, XPathResult.ANY_TYPE, null);
-        if (r == null) return null;
-
-        switch (r.resultType) {
-        case XPathResult.BOOLEAN_TYPE:
-            return r.booleanValue;
-        case XPathResult.STRING_TYPE:
-            return r.stringValue;
-        case XPathResult.NUMBER_TYPE:
-            return r.numberValue;
-        case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
-            var rr = new Array();
-            var n;
-            while ((n = r.iterateNext()) != null)
-                rr.push(n);
-            return rr;
-        default:
-            libxEnv.writeLog("unknown resultType: " + r.resultType, libxEnv.logTypes.xpath);
+        var r;
+        try {
+            r = doc.evaluate(xpathexpr, root ? root : doc, 
+                             function (prefix) { 
+                                 return namespaceresolver[prefix]; 
+                             }, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        }
+        catch (e) {
+            //XXX: Need to use a more specific log type
+            libxEnv.writeLog("In findSingleXML: XPath expression " + xpathexpr + " does not return a node");
             return null;
         }
+
+        //If there's no result, this is set to null
+        return r.singleNodeValue;
     },
 
     /**
+     * Evaluates an XPath expression and returns an array of DOM nodes
      *
+     * @param {DOM Tree} doc               document (used if root is undefined)
+     * @param {String}   xpathexpr         XPath expression
+     * @param {DOM Tree} root              root of DOM to execute search (used
+     *                                     instead of doc if defined)
+     * @param {Object}   namespaceresolver Object keys are namespace prefixes,
+     *                                     values are corresponding URIs
+     *
+     * @returns array of nodes or null
      */
+
+    findNodesXML : function (doc, xpathexpr, root, namespaceresolver) {
+        var r;
+        try {
+            r = doc.evaluate(xpathexpr, root ? root : doc, 
+                             function (prefix) { 
+                                 return namespaceresolver[prefix]; 
+                             }, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+        }
+        catch (e) {
+            //XXX: Need to use a more specific log type
+            libxEnv.writeLog("In findNodesXML: XPath expression " + xpathexpr + " does not return a set of nodes");
+            return null;
+        }
+
+        var rr = new Array();
+        var n;
+        while ((n = r.iterateNext()) != null)
+            rr.push(n);
+
+        if (rr.length == 0) return null;
+
+        return rr;
+    },
+
+    //XXX: Should this be renamed to findSnapshotXML?
+
+    /**
+     * Evaluates an XPath expression and returns an array of DOM nodes
+     *
+     * @param {DOM Tree} html              document (used if root is undefined)
+     * @param {String}   xpathexpr         XPath expression
+     * @param {DOM Tree} root              root of DOM to execute search (used
+     *                                     instead of doc if defined)
+     * @param {Object}   namespaceresolver Object keys are namespace prefixes,
+     *                                     values are corresponding URIs
+     *
+     * @returns an array of nodes
+     */
+
     findSnapshot : function (doc, xpathexpr, root, namespaceresolver) {
-        var r = doc.evaluate(xpathexpr, root?root:doc, 
-                            function (prefix) { 
-                                return namespaceresolver[prefix]; 
-                            }, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        if (r == null) return null;
+        var r;
+        try {
+            r = doc.evaluate(xpathexpr, root?root:doc, 
+                             function (prefix) { 
+                                 return namespaceresolver[prefix]; 
+                             }, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+        }
+        catch (e) {
+            //XXX: Need to use a more specific log type
+            libxEnv.writeLog("In findSnapshot: XPath expression " + xpathexpr + " does not return a set of nodes");
+            return null;
+        }
 
         var rr = new Array();
         for (var i = 0; i < r.snapshotLength; i++) {
             rr.push(r.snapshotItem(i));
         }
+        if (rr.length == 0) return null;
+
         return rr;
     }
-}
+};
 
 // XXX remove this eventually
 libxEnv.xpath = libx.ff.xpath;
