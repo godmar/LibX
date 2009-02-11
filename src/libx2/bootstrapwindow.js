b785f6d5b82bad36270b9aab3f847dc9f69add3e
godmar <godmar>
2009-02-11 06:06:29 +0000

    ...

--- src/libx2/bootstrapwindow.js
+++ src/libx2/bootstrapwindow.js
@@ -2,9 +2,12 @@
  * Load LibX code, per window
  */
 
-libx.log.write("bootstrapping libx from: " + baseUrl + " parent=" + parentRequest.url);
+libx.log.write("bootstrapping libx from: " + scriptBase.baseURL + " parent=" + scriptBase.request.url);
 var files = [
     "base/libapp.js",
     "base/atomparser.js",
 ];
 
+for (var i = 0; i < files.length; i++) {
+    scriptBase.loadDependentScript(scriptBase.baseURL + files[i]);
+}
