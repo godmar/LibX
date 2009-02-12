7c3753c33570a2e40e31ef834d0a97873d51f8c4
godmar <godmar>
2009-02-12 15:46:47 +0000

    changed comments

--- src/base/chrome/libx/content/libx/libx.global.ff.js
+++ src/base/chrome/libx/content/libx/libx.global.ff.js
@@ -2,15 +2,12 @@
  * Browser dependent definitions of global functions.
  */
 
-/**
- * @namespace
- *
+/*
  * FF-specific implementation of logging.
  */
 libx.log.bd = {
-    /**
-     * Write a message to the JS console
-     * @param {String} msg message to write
+    /*
+     * @see libx.log.bd.write
      */
     write : function (msg) {
         var consoleService = Components.classes["@mozilla.org/consoleservice;1"]
