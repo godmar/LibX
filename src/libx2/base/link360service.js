5926ae783e33be7a62706a74348abfcdafabf6de
godmar <godmar>
2010-01-14 17:43:46 +0000

    + added throttling + improved documentation

--- src/libx2/base/link360service.js
+++ src/libx2/base/link360service.js
@@ -32,8 +32,12 @@ libx.services.link360.Link360 = libx.core.Class.create(
     initialize : function (ssid) {
         this.baseURL = "http://" + ssid + ".openurl.xml.serialssolutions.com/openurlxml";
         this.ssNamespaceResolver = { 'ssopenurl' : 'http://xml.serialssolutions.com/ns/openurl/v1.0' };
+        this.pendingRequests = new Array();
+        this.requestsInProgress = 0;
     },
     /**
+     * Obtains metadata from Link/360 service based on context object
+     *
      * @param {Object} options
      * @param {String} options.query - OpenURL query (as URL fragment)
      * @param {String} options.type - type: e.g., 'journal', 'article' - optional
@@ -43,17 +47,19 @@ libx.services.link360.Link360 = libx.core.Class.create(
      */
     getMetadata : function (options) {
         var self = this;
-        libx.cache.defaultMemoryCache.get({
+        var cacheRequest = {
             url: this.baseURL + "?version=1.0&" + options.query,
             dataType: "xml",
             success: function (xmlResponse, status, xhr) {
 
                 var linkgroupquery = "//ssopenurl:result[1]//ssopenurl:linkGroup[@type = 'holding']";
 
+                // FIXME: why only first linkGroup?
                 var linkgroup1 = libx.utils.xpath.findSingleXML(xmlResponse, linkgroupquery, 
                                             xmlResponse, self.ssNamespaceResolver);
                 if (linkgroup1 == null) {
-                    options.notFound(xmlResponse);
+                    if (options.notFound)
+                        options.notFound(xmlResponse);
                 } else {
                     var typeConstraint = options.type ? "[@type = '" + options.type + "']" : "";
                     var fulltexturl = libx.utils.xpath.findSingleXML(xmlResponse, 
@@ -68,10 +74,45 @@ libx.services.link360.Link360 = libx.core.Class.create(
 
                         options.hasFullText(xmlResponse, fulltexturl.data, dbName);
                     } else {
-                        options.foundNoFullText(xmlResponse);
+                        if (options.foundNoFullText)
+                            options.foundNoFullText(xmlResponse);
                     }
                 }
+            },
+            error : function (result, status, xhr) {
+                if (options.onError)
+                    options.onError(result, status);
+
+                if (Number(status) == 503 && options._attempts < 3) {
+                    var request = this;
+                    libx.utils.timer.setTimeout(function () { startRequest(request); }, 5000);
+                }
+            },
+            complete : function () {
+                self.requestsInProgress--;
+                if (self.pendingRequests.length > 0)
+                    startRequest (self.pendingRequests.pop());
+            },
+            _options : options
+        };
+
+        function startRequest (request) {
+            /*
+             * As of Sep 2009, Link/360 seems to randomly return "Service unavailable"
+             * when hit with multiple requests.  Manage the load by limiting the
+             * number of simultaneous requests.
+             */
+            if (self.requestsInProgress < 5) {
+                self.requestsInProgress++;
+                if (request._options.onStart)
+                    request._options.onStart();
+                request._options._attempts = (request._options._attempts || 0) + 1;
+                libx.cache.defaultMemoryCache.get(request);
+            } else {
+                self.pendingRequests.push(request);
             }
-        });
+        }
+
+        startRequest(cacheRequest);
     }
 });
