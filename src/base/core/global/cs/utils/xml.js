829a03117fa3a7b1ec6854bccdef1af1f63ac454
Rupen Khandpur <rupen@cs.vt.edu>
2011-11-20 00:46:59 -0500

    IE specific functionality added for DOMParser and XMLSerializer

--- src/base/core/global/cs/utils/xml.js
+++ src/base/core/global/cs/utils/xml.js
@@ -2,11 +2,41 @@
 // implements abstract functions in core/global/shared/libx.js
 
 libx.utils.xml.loadXMLDocumentFromString = function (xmlstring) {
-    var parser = new DOMParser();
-    return parser.parseFromString(xmlstring, "text/xml");
+     var xml, parser;
+     try {
+         if ( window.DOMParser ) { // Standard
+             parser = new DOMParser();
+             xml = parser.parseFromString( xmlstring , "text/xml" );
+         } else { // IE
+             xml = new ActiveXObject( "Microsoft.XMLDOM" );
+             xml.async = "false";
+             xml.loadXML( xmlstring );
+         }
+    } catch( e ) {
+         xml = undefined;
+    }
+
+    if ( !xml || !xml.documentElement || xml.getElementsByTagName( "parsererror" ).length ) {
+         libx.log.write( "Invalid XML ==>\n" + xmlstring );
+    }
+    return xml;
 }
 
 libx.utils.xml.convertXMLDocumentToString = function (xmlDoc) {
-    var serializer = new XMLSerializer();
-    return serializer.serializeToString(xmlDoc);
+    var xmlstring, serializer;
+    try {
+        if( window.XMLSerializer ) { // standard
+            serializer = new XMLSerializer();
+            xmlstring = serializer.serializeToString(xmlDoc);
+        }else { //IE
+           xmlstring = xmlDoc.xml;
+        }
+    } catch( e ) {
+       xmlstring = undefined;
+    }
+   
+    if(!xmlstring)
+       libx.log.write("Error converting xml to string");
+
+    return xmlstring;
 }
