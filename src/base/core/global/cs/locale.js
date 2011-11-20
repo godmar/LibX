cfe45b6f2aa55329006a1233d8101047013d0ab8
Rupen Khandpur <rupen@cs.vt.edu>
2011-11-20 01:10:01 -0500

    Browser specific locale implementation

--- src/base/core/global/cs/locale.js
+++ src/base/core/global/cs/locale.js
@@ -2,5 +2,22 @@ libx.locale.bd.initialize = function () {
     libx.log.write('initializing locale...');
 };
 
-libx.locale.bd.currentLocale = 'en_US';
+var strlocale;
+try {
+  if( window.navigator.language )//standard (for chrome, FF, opera, safari)
+  {
+     strlocale = window.navigator.language;
+  }else {// IE
+     strlocale = window.navigator.userLanguage;
+  }
+  strlocale = strlocale.split("-");
+  strlocale = strlocale[0].toLowerCase() + "_" + strlocale[1].toUpperCase();
+
+}catch ( e )
+{
+  strlocale = 'en_US';//default
+  libx.log.write( "Error processing locale info: " + e);
+}
+
+libx.locale.bd.currentLocale = strlocale;
 
