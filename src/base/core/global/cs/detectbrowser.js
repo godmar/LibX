5148534cc07249b89c00c6248f6937da18b51fb2
Rupen Khandpur <rupen@cs.vt.edu>
2011-12-05 08:52:43 -0500

    addded comments

--- src/base/core/global/cs/detectbrowser.js
+++ src/base/core/global/cs/detectbrowser.js
@@ -1,29 +1,34 @@
-
+/*Defining cs.browser namespace here*/
  libx.cs.browser = {};
+/*Set the bowser type for Client-Side implementation
+* for example if Client browser is chrom then
+   libx.cs.browser.chrome will evaluate to true
+   and libx.cs.browser.[webkit]|[msie]|[opera]|[mozilla] will be unddefined
 
+ and current browser version ==> libx.cs.browser.version 
+*/
+(function () {
+  /*Browser specific regex*/
+  var rwebkit = /(webkit)[ \/]([\w.]+)/,
+      ropera = /(opera)(?:.*version)?[ \/]([\w.]+)/,
+      rmsie = /(msie) ([\w.]+)/,//IE
+      rchrome = /(chrome)[ \/]([\w.]+)/,
+      rmozilla = /(mozilla)(?:.*? rv:([\w.]+))?/,
+      ua = window.navigator.userAgent.toLowerCase();
 
- var rwebkit = /(webkit)[ \/]([\w.]+)/,
-    ropera = /(opera)(?:.*version)?[ \/]([\w.]+)/,
-    rmsie = /(msie) ([\w.]+)/,
-    rchrome = /(chrome)[ \/]([\w.]+)/,
-    rmozilla = /(mozilla)(?:.*? rv:([\w.]+))?/,
-
-    ua = window.navigator.userAgent.toLowerCase();
-
- var match = rchrome.exec( ua ) ||
-            rwebkit.exec( ua ) ||
-            ropera.exec( ua ) ||
-            rmsie.exec( ua ) ||
-            ua.indexOf("compatible") < 0 && rmozilla.exec( ua ) ||
-            [];
- if(match[1])
- {
+  var match = rchrome.exec( ua ) ||
+              rwebkit.exec( ua ) ||
+              ropera.exec( ua ) ||
+              rmsie.exec( ua ) ||
+              ua.indexOf("compatible") < 0 && rmozilla.exec( ua ) ||
+              [];
+  if(match[1])
+  {
     libx.cs.browser[match[1]] = true;
     libx.cs.browser.version = match[2] || "0";
- }
-    
- 
- if( libx.cs.browser.webkit ) {
-    libx.cs.browser.safari = true;
- }
-
+  }
+  //if not chrome but webkit is true, then its safari
+  if( libx.cs.browser.webkit ) {
+     libx.cs.browser.safari = true;
+  }
+})();
