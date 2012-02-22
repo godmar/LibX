7baa5553c5b99efa5d9cc2bf0a1412e4bed880f3
Godmar Back <gback@libx.cs.vt.edu>
2012-02-22 09:52:13 -0500

    Fix ISBN search.
    - also retabbed, use git diff -b

--- src/base/core/global/shared/catalog/factory/sirsi.js
+++ src/base/core/global/shared/catalog/factory/sirsi.js
@@ -22,11 +22,11 @@
  * ***** END LICENSE BLOCK ***** */
 
 /**
- *	Sirsi OPAC Catalog Implementation
+ *  Sirsi OPAC Catalog Implementation
  *
- *	@name libx.catalog.factory.sirsi
- *	@augments libx.catalog.Catalog
- *	@class
+ *  @name libx.catalog.factory.sirsi
+ *  @augments libx.catalog.Catalog
+ *  @class
  */
 libx.catalog.factory["sirsi"] = libx.core.Class.create(libx.catalog.Catalog, 
 /** @lends libx.catalog.factory.sirsi.prototype */
@@ -44,42 +44,46 @@ libx.catalog.factory["sirsi"] = libx.core.Class.create(libx.catalog.Catalog,
       <option value="PER^PERTITLE^TITLES^Title Processing^periodical title">periodical title
 */
 
-	convert: function (stype) {
-	    switch (stype) {
-	        case 'd':   return "&srchfield1=" + this.convert2(stype);
-	        case 'a':   return "&srchfield1=" + this.convert2(stype);
+    convert: function (stype) {
+        switch (stype) {
+            case 'd':   return "&srchfield1=" + this.convert2(stype);
+            case 'a':   return "&srchfield1=" + this.convert2(stype);
             case 'jt':
-	        case 't':   return "&srchfield1=" + this.convert2(stype);
+            case 't':   return "&srchfield1=" + this.convert2(stype);
+            /* It's not clear what to place here. Up to 2/21/12 it was return ""
+             *  mgoddard@calbaptist.edu suggests that for their installation, 'srchfield1' is required.
+             * Historically, we had return "{022}"; here.
+             */
             case 'is':
-	        case 'i':   return ""; // maybe nothing will do better here? return "{022}";
-	        case 'c':   return ""; //                                    return "{050}";
-	        case 'Y':   return "&srchfield1=" + this.convert2(stype);
-	        default:
-	            return "";
-	    }
-	},
-	convert2: function (stype) {
-	    switch (stype) {
+            case 'i':   return "&srchfield1=" + this.convert2(stype); 
+            case 'c':   return ""; //                                    return "{050}";
+            case 'Y':   return "&srchfield1=" + this.convert2(stype);
+            default:
+                return "";
+        }
+    },
+    convert2: function (stype) {
+        switch (stype) {
             case 'd':   return "SU^SUBJECT^SUBJECTS^^subject";
-	        case 'a':   return "AU^AUTHOR^AUTHORS^Author Processing^author";
-	        case 't':   return "TI^TITLE^TITLES^Title Processing^title";
-	        case 'jt':  return "PER^PERTITLE^TITLES^Title Processing^periodical title";
+            case 'a':   return "AU^AUTHOR^AUTHORS^Author Processing^author";
+            case 't':   return "TI^TITLE^TITLES^Title Processing^title";
+            case 'jt':  return "PER^PERTITLE^TITLES^Title Processing^periodical title";
             case 'is': 
-	        case 'i':   return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";    // ???
-	        case 'Y':   return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";
-	        case 'c':   return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";    // ???
-	        default:
-	            return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";
-	    }
-	},
+            case 'i':   return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";    // ???
+            case 'Y':   return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";
+            case 'c':   return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";    // ???
+            default:
+                return "GENERAL^SUBJECT^GENERAL^^keywords anywhere";
+        }
+    },
     // if sirsi supports article title searches, adjust
-	supportsSearchType: function (stype) {  
-	    if (stype == 'at') {
-	        alert(libxGetProperty("articletitle.alert"));
-			return false;
-	    }
-	    return true;
-	},
+    supportsSearchType: function (stype) {  
+        if (stype == 'at') {
+            alert(libxGetProperty("articletitle.alert"));
+            return false;
+        }
+        return true;
+    },
     // You can limit the search to a specific library
     scopeField: function (query) {
         if (this.searchscope != 1) {
@@ -102,21 +106,21 @@ libx.catalog.factory["sirsi"] = libx.core.Class.create(libx.catalog.Catalog,
         }
         return "";
     },
-	makeSearch: function(stype, sterm) {
+    makeSearch: function(stype, sterm) {
         return this.url + this.path + "?searchdata1=" + encodeURIComponent(sterm) + this.convert(stype)
             + this.scopeField() + this.sortField() + this.userId();
-	},
-	makeAdvancedSearch: function(fields) {
+    },
+    makeAdvancedSearch: function(fields) {
         var url = this.url + this.path + "?";
-		for (var i = 0; i < fields.length; i++) {
-			url += "srchfield" + (i+1) + "=" + this.convert2(fields[i].searchType) 
+        for (var i = 0; i < fields.length; i++) {
+            url += "srchfield" + (i+1) + "=" + this.convert2(fields[i].searchType) 
                 + "&searchdata" + (i+1) + "=" + encodeURIComponent(fields[i].searchTerms);
             if (i < fields.length - 1) {
                 url += "&searchoper" + (i+1) + "=AND&";
             }
-		}
-		return url + this.scopeField() + this.sortField() + this.userId();
-	}
+        }
+        return url + this.scopeField() + this.sortField() + this.userId();
+    }
 });
 
 // vim: ts=4
